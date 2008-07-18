56c65bac07596e1d05cdbd4024e964b69efb7e97
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-07-18 18:16:44 -0400

    Use Check_Type in nymble_util_wrap.

--- src/libnymble-ruby/nymble_util_wrap.c
+++ src/libnymble-ruby/nymble_util_wrap.c
@@ -1,6 +1,7 @@
 #include "nymble_util_wrap.h"
 
-VALUE rb_nymble_hash(VALUE rb_self, VALUE rb_value) {
+VALUE rb_nymble_hash(VALUE rb_self, VALUE rb_value)
+{
   rb_value      = rb_funcall(rb_value, rb_intern("to_s"), 0);
   u_char *value = (u_char *)RSTRING(rb_value)->ptr;
   u_int size    = RSTRING(rb_value)->len;
@@ -12,10 +13,9 @@ VALUE rb_nymble_hash(VALUE rb_self, VALUE rb_value) {
   return rb_str_new((char *)&buffer, sizeof(buffer));
 }
 
-VALUE rb_nymble_random_bytes(VALUE rb_self, VALUE rb_count) {
-  if (TYPE(rb_count) != T_FIXNUM) {
-    return Qnil;
-  }
+VALUE rb_nymble_random_bytes(VALUE rb_self, VALUE rb_count)
+{
+  Check_Type(rb_count, T_FIXNUM);
 
   u_int size = NUM2UINT(rb_count);
   u_char buffer[size];
