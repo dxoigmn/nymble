1657ec6403d2020d8f33b1cd37c4804699726412
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-08-11 04:34:01 -0400

    Use symbolic link for plot script.

--- src/tools/plot.rb
+++ src/tools/plot.rb
@@ -29,11 +29,25 @@ Gnuplot.open do |gp|
   Gnuplot::Plot.new(gp) do |plot|
     plot.output   "#{ARGV[1] || 'nymble_authentication.pdf'}"
     plot.terminal 'pdf fsize 9'
-    plot.title    'Nymble performance'
+    plot.title    'Nymble Performance'
     plot.ylabel   'Time (sec)'
-    plot.xlabel   'Blacklist entries'
+    plot.xlabel   'Number of blacklist entries'
     plot.xrange   '[0:2000]'
-    plot.yrange   '[0:0.01]'
+    #plot.yrange   '[0:0.18]'
+    plot.key      'top left'
+
+=begin
+    @values.each do |title, values|
+      values = values.to_a.sort_by { |x, y| x }
+      x_values = values.map { |x, y| x }
+      y_values = values.map { |x, y| y }
+      
+      plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
+        ds.title = title
+        ds.with  = 'linespoints'
+      end
+    end
+=end
 
     values = @values['user_blacklist_update_average_2']
     values   = values.to_a.sort_by { |x, y| x }
@@ -42,19 +56,9 @@ Gnuplot.open do |gp|
     
     plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
       ds.title  = 'User blacklist verify'
-      ds.with   = 'linespoints 4'
+      ds.with   = 'linespoints 1'
     end
-    
-    values = @values['server_iterate']
-    values   = values.to_a.sort_by { |x, y| x }
-    x_values = values.map { |x, y| x }
-    y_values = values.map { |x, y| y }
-    
-    plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
-      ds.title  = 'Server iterate'
-      ds.with   = 'linespoints 3'
-    end
-    
+
     values = @values['user_blacklist_check_fail_average']
     values   = values.to_a.sort_by { |x, y| x }
     x_values = values.map { |x, y| x }
@@ -62,7 +66,7 @@ Gnuplot.open do |gp|
     
     plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
       ds.title  = 'User blacklist check'
-      ds.with   = 'linespoints 1'
+      ds.with   = 'linespoints 2'
     end
     
     values = @values['server_ticket_verify_pass_average']
@@ -72,7 +76,17 @@ Gnuplot.open do |gp|
     
     plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
       ds.title  = 'Server authentication'
-      ds.with   = 'linespoints 2'
+      ds.with   = 'linespoints 3'
+    end
+    
+    values = @values['server_iterate']
+    values   = values.to_a.sort_by { |x, y| x }
+    x_values = values.map { |x, y| x }
+    y_values = values.map { |x, y| y }
+    
+    plot.data << Gnuplot::DataSet.new([x_values, y_values]) do |ds|
+      ds.title  = 'Server iterate'
+      ds.with   = 'linespoints 4'
     end
   end
 end
