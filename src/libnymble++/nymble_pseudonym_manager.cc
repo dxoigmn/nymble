0523d5454cb6a28676007660bbc3901fd36c2b7c
Cory T. Cornelius <Cory.T.Cornelius@Dartmouth.edu>
2008-11-15 04:37:19 -0500

    Get rid of hardcoded buffer sizes.

--- src/libnymble++/nymble_pseudonym_manager.cc
+++ src/libnymble++/nymble_pseudonym_manager.cc
@@ -6,9 +6,9 @@ PseudonymManager::PseudonymManager()
 {
   char nym_key_p[DIGEST_SIZE];
   
-  RAND_bytes((u_char*)nym_key_p, DIGEST_SIZE);
+  RAND_bytes((u_char*)nym_key_p, sizeof(nym_key_p));
   
-  this->nym_key_p = std::string(nym_key_p, DIGEST_SIZE);
+  this->nym_key_p = std::string(nym_key_p, sizeof(nym_key_p));
 }
 
 void PseudonymManager::setMacKeyNP(std::string mac_key_np)
@@ -21,19 +21,19 @@ bool PseudonymManager::createPseudonym(std::string user_id, Pseudonym* pseudonym
   HMAC_CTX ctx;
   u_char buffer[DIGEST_SIZE];
   
-  HMAC_Init(&ctx, this->nym_key_p.c_str(), DIGEST_SIZE, EVP_sha256());
-  HMAC_Update(&ctx, (u_char*)user_id.c_str(), DIGEST_SIZE);
+  HMAC_Init(&ctx, this->nym_key_p.c_str(), this->nym_key_p.size(), EVP_sha256());
+  HMAC_Update(&ctx, (u_char*)user_id.c_str(), this->nym_key_p.size());
   HMAC_Update(&ctx, (u_char*)&this->cur_link_window, sizeof(this->cur_link_window));
   HMAC_Final(&ctx, buffer, NULL);
   
-  pseudonym->set_nym(buffer, DIGEST_SIZE);
+  pseudonym->set_nym(buffer, sizeof(buffer));
   
-  HMAC_Init(&ctx, this->mac_key_np.c_str(), DIGEST_SIZE, EVP_sha256());
-  HMAC_Update(&ctx, buffer, DIGEST_SIZE);
+  HMAC_Init(&ctx, this->mac_key_np.c_str(), this->mac_key_np.size(), EVP_sha256());
+  HMAC_Update(&ctx, this->nym.c_str(), this->nym.size());
   HMAC_Update(&ctx, (u_char*)&this->cur_link_window, sizeof(this->cur_link_window));
   HMAC_Final(&ctx, buffer, NULL);
   
-  pseudonym->set_mac(buffer, DIGEST_SIZE);
+  pseudonym->set_mac(buffer, sizeof(buffer)););
   
   return true;
 }
